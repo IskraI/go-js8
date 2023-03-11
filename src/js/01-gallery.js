777183609ba962a834caa450c4d5e1de43f0e25b
Irina Iskra <iskrairina.a@gmail.com>
2023-03-11 23:31:32 +0200

    task1

--- src/js/01-gallery.js
+++ src/js/01-gallery.js
@@ -1,5 +1,24 @@
 // Add imports above this line
 import { galleryItems } from './gallery-items';
 // Change code below this line
-
+import SimpleLightbox from 'simplelightbox';
+import 'simplelightbox/dist/simple-lightbox.min.css';
 console.log(galleryItems);
+
+const galleryEl = document.querySelector('.gallery');
+
+const galleryItemsEl = galleryItems
+  .map(({ preview, description, original }) => {
+    return `<a class="gallery__item" href="${original}">
+  <img class="gallery__image" src="${preview}" alt="${description}" loading = "lazy"/>
+</a>`;
+  })
+  .join('');
+
+galleryEl.insertAdjacentHTML('beforeend', galleryItemsEl);
+
+const gallery = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionPosition: 'bottom',
+  captionDelay: 250,
+});
