a4e119bd8799ae9031cfd30a8c0fa9732f0c33ab
Irina Iskra <iskrairina.a@gmail.com>
2023-03-12 01:12:16 +0200

    task2

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import Player from '@vimeo/player';
+import throttle from 'lodash.throttle';
 
+const iframe = document.querySelector('iframe');
+const player = new Player(iframe);
+
+const STORAGE_TIME = 'videoplayer-current-time';
+
+const currentTime = localStorage.getItem(STORAGE_TIME)
+  ? localStorage.getItem(STORAGE_TIME)
+  : 0;
+
+player.on('timeupdate', throttle(getCurrentTime, 1000));
+
+function getCurrentTime(e) {
+  localStorage.setItem(STORAGE_TIME, e.seconds);
+}
+
+player.setCurrentTime(currentTime);
+
+player.getVideoTitle().then(function (title) {
+  console.log('title:', title);
+});
